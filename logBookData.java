95933f0386e04cae630746f817675930b0435d81
Agent-L <mohammed.rayyan.zero12@gmail.com>
2025-11-12 15:29:59 +0530

    feat:Add View and Add buttons

--- logBookData.java
+++ logBookData.java
@@ -1,14 +1,52 @@
 import javax.swing.*;
+
 import java.awt.*;
+import java.io.File;
 
 class DataPlace {
-    	DataPlace() {
-		JFrame jf = new JFrame("Data Zone");
+    JFrame jf;
+
+    DataPlace() {
+		jf = new JFrame("Data Zone");
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jf.setSize(900, 500);
 		jf.setLocationRelativeTo(null);
+
+        createInitialView();
 		jf.setVisible(true);
 	}
+
+    void createInitialView() {
+        JPanel view = new JPanel(new FlowLayout());
+        JButton addLogBook = new JButton("Add"); view.add(addLogBook);
+        JButton viewLogBook = new JButton("View"); view.add(viewLogBook);
+
+        addLogBook.addActionListener(e -> {
+            showAddLogBookPanel();
+        });
+        jf.add(view);
+    }
+
+    void showAddLogBookPanel() {
+        importFromUserSelection();
+    }
+
+    void importFromUserSelection() {
+        JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setDialogTitle("Select a file to import");
+		fileChooser.setCurrentDirectory(new File("./") );
+
+
+		int userSelection = fileChooser.showOpenDialog(null); 
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+
+			File selectedFile = fileChooser.getSelectedFile();
+
+            JOptionPane.showMessageDialog(null, "Successfully imported data from " + selectedFile.getName(),
+						"Import Complete", JOptionPane.INFORMATION_MESSAGE);
+        }
+
+    }
 }
 
 public class logBookData {
